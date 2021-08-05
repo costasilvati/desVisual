1d93f47981f9a93a50dd003e16031b0f30185636
Costa-Silva <julianacostasilvati@gmail.com>
2021-08-04 21:35:38 -0300

    Aula 1
    Arquivos gerados na aula 1

---
+++ DesenvolvimentoSoftwareVisual/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class MainClass
+    { 
+        //Sensitive case
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Olá Mundo! Executando Primeiro Projeto!");
+            Console.WriteLine("A aplicação Terminou, tecle ENTER para sair ...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesenvolvimentoSoftwareVisual/2-Variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double salario = 1580.90;
+            Console.WriteLine(salario);
+
+            //int valor = 12.5; // Não compila
+            //int teste = salario; // Não compila
+
+            int divisao = 5/2;
+            Console.WriteLine(divisao);
+
+        }
+    }
+}
