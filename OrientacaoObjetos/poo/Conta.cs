935c7ddf2af0da9bf0d19b755dc578e8f31e8546
Costa-Silva <julianacostasilvati@gmail.com>
2021-08-18 20:36:53 -0300

    Código de aula 3
    Orientação a objetos com C#

---
+++ OrientacaoObjetos/poo/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace poo
+{
+    public class Conta
+    {
+        public int numero;
+        public string cliente; // Implemente a classe Cliente
+        public float saldo;
+        public string senha;
+        public string tipo;
+
+        public bool Saque(float valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            this.saldo -= valor;
+            //this.saldo = (saldo - valor);
+            return true;
+        }
+
+        public bool Deposito(float valor)
+        {
+            if(valor > 0)
+            {
+                this.saldo += valor;
+                return true;
+            }
+            return false;
+        }
+
+        //public bool Pix(float valor, Conta contaDestino)
+        //{
+            
+        //}
+    }
+}
---
+++ OrientacaoObjetos/poo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace poo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Banco");
+
+            // Instanciando Objeto Conta
+            Conta conta1 = new Conta();
+            conta1.numero = 123456;
+            conta1.cliente = "Juliana Costa";
+            conta1.saldo = 10000;
+            conta1.tipo = "Corrente";
+            conta1.senha = "123456";
+            Console.WriteLine("Conta: " + conta1.cliente);
+
+            Conta conta2 = new Conta();
+            conta2 = conta1;
+            Console.WriteLine("Igualdade de tipos de referência " + (conta1 == conta2));
+
+            // Métodos
+            bool saque = conta1.Saque(15000);
+            if (saque)
+            {
+                Console.WriteLine("Saque efetuado. Novo saldo: R$" + conta1.saldo);
+            }
+            else
+            {
+                Console.WriteLine("Saldo induficiente!");
+            }
+
+            if (conta1.Deposito(300000))
+            {
+                Console.WriteLine("Depósito Efetuado! Novo saldo: R$" + conta1.saldo);
+            }
+            else
+            {
+                Console.WriteLine("Valor de depósito deve ser maior que 0");
+            }
+
+            //Implemente o método Pix na classe Conta
+            // e teste o método AQUI
+        }
+    }
+}
