fb6b543b73fa2571df25e5eae911c96915ddef4b
Costa-Silva <julianacostasilvati@gmail.com>
2021-09-01 23:13:31 -0300

    Aula 5 - código
    C# - OO Herança

---
+++ OOHeranca/poo/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace poo
+{
+    public class Cliente
+    {
+        private string nome;
+        private string cpf;
+        private DateTime nascimento;
+        private string profissao;
+        private string empresa;
+        private string pai;
+        private string mae;
+        // Classe endereco implementar e outras necessárias
+        public string logradouro;
+        public int numero;
+        public string bairro;
+        public string cidade;
+        public string cep;
+        public string uf;
+        private string email;
+        private string fone;
+
+
+        /*
+         * Atalho
+         * prop + tab + tab
+         * tipo do atributo
+         * nome do atributo
+         * ENTER
+         */
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string Profissao { get; set; }
+        public string Empresa { get; set; }
+        public string Pai { get; set; }
+        public string Mae { get; set; }
+
+    }
+}
