6290d1b91e1de33ad9af566949c0c0d1a97b4be4
Costa-Silva <costasilvati@users.noreply.github.com>
2021-09-03 17:55:54 -0300

    Código atualizado

---
+++ OrientacaoAObjetos/OrientacaoAObjetos/Funcionarios/Diretor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OrientacaoAObjetos.Funcionarios
+{
+    public class Diretor : Funcionario //herança
+    {
+        public override double GetBonificacao() //Sobrescrita de método da super classe
+        {
+            return Salario;
+        }
+    }
+}
---
+++ OrientacaoAObjetos/OrientacaoAObjetos/Funcionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace OrientacaoAObjetos.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+        private int _tipo;
+
+        public Funcionario (int tipo)
+        {
+            this._tipo = tipo;
+        }
+
+        public Funcionario() { }
+
+        public virtual double GetBonificacao() // é possível que outra classe mude seu comortamento
+        {
+            // 0 - funcionario
+            // 1 - diretor
+            // 2 - designer
+            if(this._tipo == 1)
+            {
+                return Salario;
+            }
+            return Salario * 0.10;
+        }
+
+    }
+}
