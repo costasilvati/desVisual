a6b29c99f6df95855a1cfa930037594c915130ad
Costa-Silva <julianacostasilvati@gmail.com>
2021-10-07 19:08:28 -0300

    Código da aula 10

---
+++ CardAPI/CardAPI/Model/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardAPI.Model
+{
+    public class Card
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Number { get; set; }
+        [Required]
+        public DateTime ExprirationDate { get; set; }
+        public bool Blocked { get; set; }
+        [Required]
+        public DateTime UpdateDate { get; set; }
+    }
+}
