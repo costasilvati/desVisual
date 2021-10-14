f89c6bb9c42f30c398828ef1516118a8439073c9
Costa-Silva <julianacostasilvati@gmail.com>
2021-10-14 18:15:21 -0300

    Código de aula 11
    Conexão com banco de dados. Insert e select

---
+++ CardAPI/CardAPI/Migrations/20211013232334_createDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace CardAPI.Migrations
+{
+    public partial class createDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Number = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: false),
+                    ExprirationDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Blocked = table.Column<int>(type: "int", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
---
+++ CardAPI/CardAPI/Model/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardAPI.Model
+{
+    public class Card
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Number need be 16 characters")]
+        [StringLength(16, ErrorMessage ="Number can't be lower of 16 characters")]
+        public string Number { get; set; }
+        [Required(ErrorMessage ="Field ExpirationDate is required")]
+        public DateTime ExprirationDate { get; set; }
+        //[Required]
+        //[Range(1,10, ErrorMessage="O valor deve ser entre 1 e 10")]
+        public bool Blocked { get; set; }
+        [Required]
+        public DateTime UpdateDate { get; set; }
+    }
+}
