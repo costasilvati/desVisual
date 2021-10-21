0e3e1748aa337afdca52f0dd268460175d8916c3
Costa-Silva <julianacostasilvati@gmail.com>
2021-10-20 21:03:38 -0300

    aula 12 - código

---
+++ CardAPI/CardAPI/Data/Dtos/CreateCardDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardAPI.Data.Dtos
+{
+    public class CreateCardDto
+    {
+        [Required(ErrorMessage = "Number need be 16 characters")]
+        [StringLength(16, ErrorMessage = "Number can't be lower of 16 characters")]
+        public string Number { get; set; }
+        [Required(ErrorMessage = "Field ExpirationDate is required")]
+        public DateTime ExprirationDate { get; set; }
+        public bool Blocked { get; set; }
+        [Required]
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ CardAPI/CardAPI/Profiles/CardProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using CardAPI.Data.Dtos;
+using CardAPI.Model;
+
+namespace CardAPI.Profiles
+{
+    public class CardProfile : Profile
+    {
+        public CardProfile()
+        {
+            CreateMap<CreateCardDto, Card>();
+            CreateMap<Card, ReadCardDto>();
+            CreateMap<UpdateCardDto, Card>();
+        }
+    }
+}
