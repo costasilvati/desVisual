06de0b41cc0c495b1a75880673e8624116c4e75c
Costa-Silva <julianacostasilvati@gmail.com>
2022-08-04 16:22:03 -0300

    código de aula6

---
+++ aula6/EnderecosGerais/Estado.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OrientacaoAObjetos.Enderecos
+{
+    public class Estado
+    {
+        public string NomeEstado { get; set; }
+        public string Sigla { get; set; }
+        public string Pais { get; set; }
+    }
+}
