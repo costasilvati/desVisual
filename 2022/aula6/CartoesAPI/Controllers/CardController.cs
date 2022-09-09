6dba56508f46804980313ef2f3bc0b9dbbffe135
Costa-Silva <julianacostasilvati@gmail.com>
2022-09-08 21:19:41 -0300

    código de aula 6

---
+++ 2022/aula6/CartoesAPI/Controllers/CardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using CartoesAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CartoesAPI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class CardController : ControllerBase
+    {
+        private static List<Card> cards = new List<Card>();
+
+        [HttpPost]
+        public void AddCard([FromBody] Card card)
+        {
+            cards.Add(card);
+            Console.WriteLine(card.CardNumber);
+        }
+
+        [HttpGet]
+        public IEnumerable<Card> Get()
+        {
+            return cards;
+        }
+
+    }
+}
---
+++ 2022/aula6/CartoesAPI/Model/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CartoesAPI.Model
+{
+    public class Card
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public DateTime ExpiratenDate { get; set; }
+        [Required]
+        public string SecurityCode { get; set; }
+        public bool Blocked { get; set; }
+
+        /*
+         * {
+  "Id":1,
+  "CardNumber":"990909900",
+  "ExpirationDate":"2022-10-01",
+  "SecurityCode":"222",
+  "Blocked":"True"
+}
+        */
+        public Card()
+        {
+        }
+    }
+}
