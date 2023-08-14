194bebaed66543693382d044629586a8ede809a7
Juliana Costa-Silva <julianacostasilvati@gmail.com>
2023-08-14 17:23:36 -0300

    aula 2

---
+++ 2023/Aula2/aula2/Banco/Banco/Program.cs
@@ -0,0 +1,25 @@
+﻿using Banco;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ContaCorrente c1 = new ContaCorrente();
+        c1.titular = "12345678900";
+        c1.saldo = 100;
+
+        c1.Depositar(-1);
+        Console.WriteLine("Saldo de " + 
+            c1.titular + " é " + c1.saldo);//100
+        c1.Depositar(200);
+        Console.WriteLine("Saldo de " +
+           c1.titular + " é " + c1.saldo);//300
+        c1.Sacar(350);
+        Console.WriteLine("Saldo de " +
+           c1.titular + " é " + c1.saldo);//300
+        c1.Sacar(50);
+        Console.WriteLine("Saldo de " +
+           c1.titular + " é " + c1.saldo); //250
+
+    }
+}
---
+++ 2023/Aula2/aula2/aula2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace aula2;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Digte a idade ");
+        int idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Acompanhado? \n false - Não | true - Sim");
+        bool acompanhado = bool.Parse(Console.ReadLine());
+        if(idade >= 18 && acompanhado == true)
+        {
+            Console.WriteLine("Acesso autorizado!");
+        }
+        else
+        {
+            Console.WriteLine("Acesso NÃO autorizado");
+        }
+        */
+
+        /*
+        Console.WriteLine("Digite o valor de saque");
+        double saque = double.Parse(Console.ReadLine());
+        int[] notas = new int[] {100, 50, 20, 10, 5, 2}; 
+        for (int i = 0; i < notas.Length; i++)
+        {
+            int qtdNotas = (int) saque / notas[i];
+            saque = saque - (qtdNotas * notas[i]);
+            Console.WriteLine("Notas de " + notas[i] + 
+                ": " + qtdNotas);
+        }
+        */
+        /*
+        int valor = 0;
+        while(valor <= 100)
+        {
+            if(valor%3 == 0)
+            {
+                Console.Write(valor + " - ");
+            }
+            valor++;
+        }
+        */
+        Console.WriteLine("Digite um número para ver sua tabuada");
+        int numero = int.Parse(Console.ReadLine());
+        int i = 0;
+        do
+        {
+            i++; 
+            Console.WriteLine(numero + " x " + i + " = " + (numero*i));
+        } while (i < 10);
+
+        Console.ReadLine(); // Aguarada uma tecla para enecrrar.
+    }
+}
