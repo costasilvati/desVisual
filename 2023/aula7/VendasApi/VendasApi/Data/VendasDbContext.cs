e2f84a41e67534f1a00870fe40fcf21020f9e878
Juliana Costa-Silva <julianacostasilvati@gmail.com>
2023-09-11 22:00:02 -0300

    Conexão com banco de dados

---
+++ 2023/aula7/VendasApi/VendasApi/Data/VendasDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendasApi.Models;
+
+namespace VendasApi.Data
+{
+    public class VendasDbContext : DbContext
+    {
+        public VendasDbContext(DbContextOptions<VendasDbContext> options): base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ 2023/aula7/VendasApi/VendasApi/Migrations/20230911230450_inicial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VendasApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: true),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Fone = table.Column<string>(type: "longtext", nullable: true),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
