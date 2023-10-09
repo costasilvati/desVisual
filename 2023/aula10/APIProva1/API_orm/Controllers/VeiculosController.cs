e2f9e907d64995a6f9056fb5a0eabc693a3d539e
Juliana Costa-Silva <julianacostasilvati@gmail.com>
2023-10-09 19:21:07 -0300

    add: api zip

---
+++ 2023/aula10/APIProva1/API_orm/Controllers/VeiculosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIExemplo.Data;
+using APIExemplo.Models;
+
+namespace APIExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculosController : ControllerBase
+    {
+        private readonly ExemploContext _context;
+
+        public VeiculosController(ExemploContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Veiculos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Veiculo>>> GetVeiculos()
+        {
+          if (_context.Veiculos == null)
+          {
+              return NotFound();
+          }
+            return await _context.Veiculos.ToListAsync();
+        }
+
+        // GET: api/Veiculos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Veiculo>> GetVeiculo(int id)
+        {
+          if (_context.Veiculos == null)
+          {
+              return NotFound();
+          }
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            return veiculo;
+        }
+
+        // PUT: api/Veiculos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVeiculo(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.IdVeiculo)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(veiculo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VeiculoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Veiculos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Veiculo>> PostVeiculo(Veiculo veiculo)
+        {
+          if (_context.Veiculos == null)
+          {
+              return Problem("Entity set 'ExemploContext.Veiculos'  is null.");
+          }
+            if(veiculo.Ano < 1930)
+            {
+                return Problem("Ano deve ser maior que 1930");
+            }
+            _context.Veiculos.Add(veiculo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVeiculo", new { id = veiculo.IdVeiculo }, veiculo);
+        }
+
+        // DELETE: api/Veiculos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVeiculo(int id)
+        {
+            if (_context.Veiculos == null)
+            {
+                return NotFound();
+            }
+            var veiculo = await _context.Veiculos.FindAsync(id);
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Veiculos.Remove(veiculo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VeiculoExists(int id)
+        {
+            return (_context.Veiculos?.Any(e => e.IdVeiculo == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ 2023/aula10/APIProva1/API_orm/Data/ExemploContext.cs
@@ -0,0 +1,15 @@
+﻿using APIExemplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIExemplo.Data
+{
+    public class ExemploContext: DbContext
+    {
+        //Construtor
+        public ExemploContext(
+            DbContextOptions<ExemploContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Medicamento> Medicamentos { get; set; }
+    }
+}
